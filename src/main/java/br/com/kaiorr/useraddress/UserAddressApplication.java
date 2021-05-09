f9e35dbc07a564e1e262eaeaeb4746318f9f999d
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-05-09 16:44:42 -0300

    Initial Commit

---
+++ src/main/java/br/com/kaiorr/useraddress/UserAddressApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaiorr.useraddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAddressApplication.class, args);
+	}
+
+}
